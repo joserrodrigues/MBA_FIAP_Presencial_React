037d5fa7870551ca1a0794b4a17c2845c1f3e70d
rubens Rodrigues <rubens@schoolguardian.app>
2022-02-02 22:21:14 -0300

    Add PWA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import serviceWorker from './serviceworker';
 
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Routes/routes";
@@ -22,3 +23,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.registerServiceWorker();
